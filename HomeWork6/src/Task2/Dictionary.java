bb602da6bef0e29398a1df93631e22a95ee087b0
Marat <108996479+Marat1988@users.noreply.github.com>
2023-07-24 17:08:17 +0200

    CREATE HomeWork6

---
+++ HomeWork6/src/Task2/Dictionary.java
@@ -0,0 +1,13 @@
+package Task2;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Dictionary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        TreeMap<String, HashSet<String>> dictionary = new TreeMap<>();
+
+    }
+}
