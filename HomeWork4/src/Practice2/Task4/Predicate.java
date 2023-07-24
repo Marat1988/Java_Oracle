70dc753383a4639a5b6aa13b77f4be8fcb9113e7
Marat <108996479+Marat1988@users.noreply.github.com>
2023-07-24 10:28:09 +0200

    UPDATE

---
+++ HomeWork4/src/Practice2/Task4/Predicate.java
@@ -0,0 +1,6 @@
+package Practice2.Task4;
+
+@FunctionalInterface
+public interface Predicate {
+    boolean test(int a);
+}
--- HomeWork4/src/Practice2/Task4/Task4.java
+++ HomeWork4/src/Practice2/Task4/Task4.java
@@ -7,19 +7,20 @@ public class Task4 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         System.out.println("Исходный массив: " + Arrays.toString(arr));
-        int evenSum = Arrays.stream(arr).filter(item -> item % 2 == 0).sum(); //Четные
-        int notEvenSum = Arrays.stream(arr).filter(item -> item % 2 != 0).sum(); //Не четные
-        int rangeNumberSum = Arrays.stream(arr).filter(item -> item >= 4 && item <= 7).sum(); //В диапазоне от 4 до 7
+        System.out.println("Сумма четных элементов равна: " + Calc(arr, (item) -> item % 2 == 0));
+        System.out.println("Сумма не четных элементов равна: " + Calc(arr, (item) -> item % 2 != 0));
+        System.out.println("Сумма элементов, которые находятся в диапазоне от 4 до 7 включительно равно: " + Calc(arr, (item) -> item >= 4 && item <= 7));
         int userParam = 5;
-        int chooseUserParamNumberSum = Arrays.stream(arr).filter(item -> item % userParam == 0).sum(); //Проверка на кратность параметру
-
-        System.out.println("Сумма четных элементов равна: " + evenSum);
-        System.out.println("Сумма не четных элементов равна: " + notEvenSum);
-        System.out.println("Сумма элементов, которые находятся в диапазоне от 4 до 7 включительно равно: " + rangeNumberSum);
-        System.out.println("Сумма элементов, кратных " + userParam + " равно: " + chooseUserParamNumberSum);
-
-
+        System.out.println("Сумма элементов, кратных " + userParam + " равно: " + Calc(arr, (item) -> item % userParam == 0));
     }
 
-
+    public static int Calc(int[] arr, Predicate predicate) {
+        int sum = 0;
+        for (int j : arr) {
+            if (predicate.test(j)) {
+                sum += j;
+            }
+        }
+        return sum;
+    }
 }
