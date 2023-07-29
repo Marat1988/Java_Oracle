7c1b112c7eec7edcc8b201cb661d63e580aec24c
Marat <pirat03071988@mail.ru>
2023-07-30 00:24:16 +0200

    UPDATE

--- HomeWork6/src/Task2/Task2.java
+++ HomeWork6/src/Task2/Task2.java
@@ -1,41 +1,40 @@
 package Task2;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.Predicate;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-public class MyDictionary {
-    public static HashMap<Word, TreeSet<String>> wordStringHashMap = new HashMap<>();
+public class Task2 {
+    public static HashMap<Word, TreeSet<String>> dictionary = new HashMap<>();
 
     public static void main(String[] args) {
-        wordStringHashMap.put(new Word("выбирать", (int) (Math.random() * 10)), new TreeSet<>(Arrays.asList("select", "choose")));
-        wordStringHashMap.put(new Word("билет", (int) (Math.random() * 10)), new TreeSet<>(List.of("ticket")));
-        wordStringHashMap.put(new Word("зима", (int) (Math.random() * 10)), new TreeSet<>(List.of("winter")));
-        wordStringHashMap.put(new Word("кот", (int) (Math.random() * 10)), new TreeSet<>(List.of("cat")));
-        wordStringHashMap.put(new Word("мама", (int) (Math.random() * 10)), new TreeSet<>(List.of("mother")));
-        wordStringHashMap.put(new Word("молоко", (int) (Math.random() * 10)), new TreeSet<>(List.of("milk")));
-        wordStringHashMap.put(new Word("игра", (int) (Math.random() * 10)), new TreeSet<>(List.of("game")));
-        wordStringHashMap.put(new Word("пират", (int) (Math.random() * 10)), new TreeSet<>(List.of("pirate")));
-        wordStringHashMap.put(new Word("автомобиль", (int) (Math.random() * 10)), new TreeSet<>(List.of("car")));
-        wordStringHashMap.put(new Word("море", (int) (Math.random() * 10)), new TreeSet<>(List.of("sea")));
-        wordStringHashMap.put(new Word("письмо", (int) (Math.random() * 10)), new TreeSet<>(List.of("letter")));
+        dictionary.put(new Word("выбирать", (int) (Math.random() * 10)), new TreeSet<>(Arrays.asList("select", "choose")));
+        dictionary.put(new Word("билет", (int) (Math.random() * 10)), new TreeSet<>(List.of("ticket")));
+        dictionary.put(new Word("зима", (int) (Math.random() * 10)), new TreeSet<>(List.of("winter")));
+        dictionary.put(new Word("кот", (int) (Math.random() * 10)), new TreeSet<>(List.of("cat")));
+        dictionary.put(new Word("мама", (int) (Math.random() * 10)), new TreeSet<>(List.of("mother")));
+        dictionary.put(new Word("молоко", (int) (Math.random() * 10)), new TreeSet<>(List.of("milk")));
+        dictionary.put(new Word("игра", (int) (Math.random() * 10)), new TreeSet<>(List.of("game")));
+        dictionary.put(new Word("пират", (int) (Math.random() * 10)), new TreeSet<>(List.of("pirate")));
+        dictionary.put(new Word("автомобиль", (int) (Math.random() * 10)), new TreeSet<>(List.of("car")));
+        dictionary.put(new Word("море", (int) (Math.random() * 10)), new TreeSet<>(List.of("sea")));
+        dictionary.put(new Word("письмо", (int) (Math.random() * 10)), new TreeSet<>(List.of("letter")));
         showMenu();
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            System.out.print("Выберите пункт меню: ");
             int chooseUser = scanner.nextInt();
             if (chooseUser == 11) {
                 break;
             } else {
                 switch (chooseUser) {
-                    case 1 -> showWord(wordStringHashMap.size(), true);
+                    case 1 -> showWord(dictionary.size(), true);
                     case 2 -> insertWordAndTranslate();
                     case 3 -> updateWord();
                     case 4 -> deleteWord();
                     case 5 -> findTranslateWord();
+                    case 6 -> addTranslateWord();
+                    case 7 -> updateTranslateWord();
+                    case 8 -> clearAllTranslateWord();
                     case 9 -> showWord(10, true);
                     case 10 -> showWord(10, false);
                     default -> System.out.println("Не корректный выбор пункта меню");
@@ -69,43 +68,39 @@ public class MyDictionary {
         System.out.print("Введите перевод слова: ");
         String userTranslate = scanner.next();
         try {
-            if (wordStringHashMap.containsKey(new Word(userWord))) {
+            if (dictionary.containsKey(new Word(userWord))) {
                 System.out.println("Слово уже присутствует в словаре");
             } else {
-                wordStringHashMap.put(new Word(userWord), new TreeSet<>(List.of(userTranslate)));
-                System.out.println("Слово и перевод успешно добавлено в словарь");
+                dictionary.put(new Word(userWord), new TreeSet<>(List.of(userTranslate)));
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
-        scanner.close();
     }
 
     public static void updateWord() { //Для обновления слова
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите слово для обновления: ");
         String userWord = scanner.next();
-        if (wordStringHashMap.containsKey(new Word(userWord))) {
+        if (dictionary.containsKey(new Word(userWord))) {
             try {
                 System.out.print("Введите новое слово: ");
                 String newUserWord = scanner.next();
-                Optional<Map.Entry<Word, TreeSet<String>>> findWord = wordStringHashMap
+                Optional<Map.Entry<Word, TreeSet<String>>> findWord = dictionary
                         .entrySet()
                         .stream()
                         .filter(item -> item.getKey().getWord().equals(userWord))
                         .findFirst();
                 Word updateWord = new Word(newUserWord, findWord.get().getKey().getPopularCount());
                 TreeSet<String> translateWords = findWord.get().getValue();
-                wordStringHashMap.remove(new Word(userWord));
-                wordStringHashMap.put(updateWord, translateWords);
-                System.out.println("Обновление слова выполнено.");
+                dictionary.remove(new Word(userWord));
+                dictionary.put(updateWord, translateWords);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         } else {
             System.out.println("Искомое слово не найдено");
         }
-        scanner.close();
     }
 
     public static void deleteWord() { //Для удаления слова
@@ -113,24 +108,78 @@ public class MyDictionary {
         System.out.print("Введите слово для удаления: ");
         String wordFindUser = scanner.next();
         try {
-            if (wordStringHashMap.containsKey(new Word(wordFindUser))) {
-                wordStringHashMap.remove(new Word(wordFindUser));
-                System.out.println("Удаление слова выполнено");
+            if (dictionary.containsKey(new Word(wordFindUser))) {
+                dictionary.remove(new Word(wordFindUser));
             } else {
                 System.out.println("Искомое слово для удаления не найдено");
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        scanner.close();
+    }
+
+    public static void addTranslateWord() { //Для добавления перевода слова
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово для поиска: ");
+        String wordFindUser = scanner.next();
+        try {
+            if (dictionary.containsKey(new Word(wordFindUser))) {
+                System.out.print("Введите перевод слова для добавления: ");
+                String wordTranslate = scanner.next();
+                dictionary.get(new Word(wordFindUser)).add(wordTranslate);
+            } else {
+                System.out.println("Искомое слово не найдено");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void updateTranslateWord() { //Для обновления перевода слова
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово для поиска: ");
+        String wordFindUser = scanner.next();
+        try {
+            if (dictionary.containsKey(new Word(wordFindUser))) {
+                System.out.print("Введите перевод слова для обновления: ");
+                String findTranslateInWord = scanner.next();
+                if (dictionary.get(new Word(wordFindUser)).contains(findTranslateInWord)) {
+                    System.out.print("Введите новый перевод слова: ");
+                    String newTranslateWord = scanner.next();
+                    dictionary.get(new Word(wordFindUser)).remove(findTranslateInWord);
+                    dictionary.get(new Word(wordFindUser)).add(newTranslateWord);
+                } else {
+                    System.out.println("Перевод у данного слова не найден!");
+                }
+            } else {
+                System.out.println("Искомое слово не найдено");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void clearAllTranslateWord() { //Для удаления всех переводов слова
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите слово для поиска: ");
+        String wordFindUser = scanner.next();
+        try {
+            if (dictionary.containsKey(new Word(wordFindUser))) {
+                dictionary.get(new Word(wordFindUser)).clear();
+            } else {
+                System.out.println("Искомое слово не найдено");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void findTranslateWord() { //Для поиска перевода слова
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите слово для поиска: ");
         String userWord = scanner.next();
-        if (wordStringHashMap.containsKey(new Word(userWord))) {
-            Optional<Map.Entry<Word, TreeSet<String>>> findWord = wordStringHashMap
+        if (dictionary.containsKey(new Word(userWord))) {
+            Optional<Map.Entry<Word, TreeSet<String>>> findWord = dictionary
                     .entrySet()
                     .stream()
                     .filter(item -> item.getKey().getWord(userWord)) //Для увеличения счетчика обращений. Считаю, что если вызван метод getWord с передачей параметра, то счетчик обращений в классе увеличивается
@@ -139,7 +188,6 @@ public class MyDictionary {
         } else {
             System.out.println("Искомое слово в словаре не найдено");
         }
-        scanner.close();
     }
 
     //Отображение слов.
@@ -147,7 +195,7 @@ public class MyDictionary {
     // boolean orderPopular - сортировать по популярности слов на основе счетчика обращений
     public static void showWord(long limit, boolean orderPopular) {
         System.out.println("===================================================================");
-        wordStringHashMap
+        dictionary
                 .entrySet()
                 .stream()
                 .sorted((o1, o2) -> Integer.compare(orderPopular ? o2.getKey().getPopularCount() : o1.getKey().getPopularCount(), orderPopular ? o1.getKey().getPopularCount() : o2.getKey().getPopularCount()))
---
+++ HomeWork6/src/Task3/Task3.java
@@ -0,0 +1,7 @@
+package Task3;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+    }
+}
