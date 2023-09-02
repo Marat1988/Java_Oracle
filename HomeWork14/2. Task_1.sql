284d0503a68d04d87cf2e92051dc77ecbfe4df5f
Marat <pirat03071988@mail.ru>
2023-09-02 11:52:09 +0200

    UPDATE

---
+++ HomeWork14/2. Task_1.sql
@@ -0,0 +1,34 @@
+﻿/*Сначала добавим тестовый заказ*/
+INSERT INTO ORDERS (DateOrder, CustomerId, EmployeeId)
+SELECT CURRENT_DATE AS DateOrder, 2 AS CustomerId, 1 AS EmployeeId
+FROM dual;
+
+/*Добавление информации о новом заказе кофе*/
+INSERT INTO LineOrder (OrderId, PriceProduct, PriceClient, ProductId, Quantity)
+SELECT 1 AS OrderId, NVL(p.price, tp.price) AS PriceProduct, NVL(p.price, tp.price) AS PriceClient, p.productid, 4 AS Quantity
+FROM SubGroupProduct sb
+INNER JOIN typeproduct tp ON sb.subgroupid=tp.subgroupid
+INNER JOIN Product p ON tp.typeid=p.typeid
+WHERE sb.name='Кофеи'
+ORDER BY NVL(p.price, tp.price) DESC
+FETCH FIRST 1 ROWS ONLY;
+
+/*Добавление информации о новом заказе десерта*/
+INSERT INTO LineOrder (OrderId, PriceProduct, PriceClient, ProductId, Quantity)
+SELECT 1 AS OrderId, NVL(p.price, tp.price) AS PriceProduct, NVL(p.price, tp.price) AS PriceClient, p.productid, 4 AS Quantity
+FROM GroupProduct gp
+INNER JOIN SubGroupProduct sb ON gp.groupid=sb.groupid
+INNER JOIN typeproduct tp ON sb.subgroupid=tp.subgroupid
+INNER JOIN Product p ON tp.typeid=p.typeid
+WHERE gp.name='Десерты'
+ORDER BY NVL(p.price, tp.price) DESC
+FETCH FIRST 1 ROWS ONLY;
+
+/*Добавление информации о графике работы в ближайший понедельник*/
+INSERT INTO Schedule (ScheduleDate, TimeBegin, TimeEnd)
+SELECT NEXT_DAY(CURRENT_DATE, 'Понедельник') AS ScheduleDate,  '0 8:00:00' AS TimeBegin,  '0 18:10:00' AS TimeEnd
+FROM Dual;
+
+/*Добавление информации о новом виде кофе*/
+INSERT INTO TypeProduct (NameRus, NameEng, SubGroupId, Price)
+VALUES ('Холодные кофейные напитки', 'Cold coffee drinks', 1, 49.56);
---
+++ HomeWork14/3. Task_2.sql
@@ -0,0 +1,21 @@
+﻿/*Сначала добавим расписание на ближайший вторник*/
+INSERT INTO Schedule (ScheduleDate, TimeBegin, TimeEnd)
+SELECT NEXT_DAY(CURRENT_DATE, 'Вторник') AS ScheduleDate,  '0 8:00:00' AS TimeBegin,  '0 18:10:00' AS TimeEnd
+FROM Dual;
+
+/*Изменить расписание работы на ближайший вторник*/
+UPDATE Schedule SET TimeBegin='0 9:00:00',
+                    TimeEnd='0 17:00:00'
+WHERE to_date(ScheduleDate,'dd.mm.yy')=to_date(NEXT_DAY(CURRENT_DATE, 'Вторник'),'dd.mm.yy');
+
+/*Изменить название уже существующего вида кофе*/
+UPDATE TypeProduct SET NameRus='Кофейные напитки с молочной примесью'
+WHERE NameRus='Кофейные напитки с молоком';
+
+/*Изменить  информацию в существующем заказе*/
+UPDATE LineOrder SET Quantity=5 WHERE OrderId=1 AND ProductId=3;
+
+/*Изменит название уже существующего дисерта.*/
+/*Торт «Ленинградский» входит в десерты. Поэтому меняю его название*/
+UPDATE Product SET NameRus='Торт "Балтийский"', NameEng='cake "Baltic"'
+WHERE NameRus='Торт «Ленинградский»';
