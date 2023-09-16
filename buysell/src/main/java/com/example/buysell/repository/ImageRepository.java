0369ceb59d1a3aac41979625767e2463e9a837cc
Marat <pirat03071988@mail.ru>
2023-09-16 17:09:46 +0200

    h

--- buysell/src/main/java/com/example/buysell/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.buysell.repository;
-
-import com.example.buysell.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
