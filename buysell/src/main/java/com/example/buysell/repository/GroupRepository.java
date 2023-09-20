159480303fb3ecf9e80e4ff12c360f35d162ec28
Marat <pirat03071988@mail.ru>
2023-09-21 01:38:17 +0200

    h

--- buysell/src/main/java/com/example/buysell/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.buysell.repository;
-
-import com.example.buysell.models.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
--- buysell/src/main/java/com/example/buysell/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.buysell.repository;
-
-import com.example.buysell.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
-}
